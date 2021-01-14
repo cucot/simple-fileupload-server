da60cbb9d2cc083eafe39a5bdb9b71c625c99eb9
cucot <phucnn.work@gmail.com>
2021-01-14 22:49:32 +0700

    test accesst token

--- upload.php
+++ upload.php
@@ -10,4 +10,5 @@ $target_dir="";
 $target_file= $target_dir . basename($_FILES["fileToUpload"]["name"]);
 move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file);
 echo "xong rui ne";
+
 ?>
